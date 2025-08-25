e863e5d94eadb1bef7db9f68aa437a8ca4cb2f7a
felixhir <felix.hirschel@outlook.de>
2025-08-25 18:09:00 +0200

    Updates

---
+++ _next/static/SIVF-eANrceEuEgFm4v0B/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/e6065151585c84b7.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/ticket-generator//_next/:path+",
+        "destination": "/ticket-generator/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
