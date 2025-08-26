c535f6a05f4e0997154c516a1882772fca11af4d
felixhir <felix.hirschel@outlook.de>
2025-08-26 22:42:29 +0200

    Updates

--- _next/static/chunks/2d1649294d88d25f.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/c28176eeb0ccabe9.js",
-  "static/chunks/turbopack-0f5a6910c4353c0c.js"
-])
--- _next/static/chunks/35789ff842e078a2.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/c28176eeb0ccabe9.js",
-  "static/chunks/turbopack-30ab2cfeb6df853f.js"
-])
