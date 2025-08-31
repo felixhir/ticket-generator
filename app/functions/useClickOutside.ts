bd2f157a57085f19ee040980e18ad352099c67f8
Jonas Scholl <jonas@scholl.tech>
2025-09-01 00:41:34 +0200

    fix linting issues

--- app/functions/useClickOutside.ts
+++ app/functions/useClickOutside.ts
@@ -1,7 +1,7 @@
-import { useEffect } from 'react'
+import { RefObject, useEffect } from 'react'
 
 // Improved version of https://usehooks.com/useOnClickOutside/
-const useClickOutside = (ref: any, handler: any) => {
+const useClickOutside = (ref: RefObject<HTMLElement | null>, handler: (event: Event) => void) => {
     useEffect(() => {
         let startedInside = false
         let startedWhenMounted = false
@@ -10,14 +10,14 @@ const useClickOutside = (ref: any, handler: any) => {
             // Do nothing if `mousedown` or `touchstart` started inside ref element
             if (startedInside || !startedWhenMounted) return
             // Do nothing if clicking ref's element or descendent elements
-            if (!ref.current || ref.current.contains(event.target)) return
+            if (!ref.current || ref.current.contains(event.target as Node)) return
 
             handler(event)
         }
 
         const validateEventStart = (event: Event) => {
-            startedWhenMounted = ref.current
-            startedInside = ref.current && ref.current.contains(event.target)
+            startedWhenMounted = !!ref.current
+            startedInside = !!ref.current && ref.current.contains(event.target as Node)
         }
 
         document.addEventListener('mousedown', validateEventStart)
