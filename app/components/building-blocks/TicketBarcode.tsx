85ba5f58b474d14f700320f0138a3ea05d221ab5
Jonas Scholl <jonas@scholl.tech>
2025-09-14 17:04:38 +0200

    improve ticket barcode

--- app/components/building-blocks/TicketBarcode.tsx
+++ app/components/building-blocks/TicketBarcode.tsx
@@ -1,7 +1,7 @@
 import getFontSize, { FontSize } from '@/app/functions/getFontSize'
 import getTicketTextColor, { TicketColor } from '@/app/functions/getTicketTextColor'
 
-import { useEffect, useMemo, useState } from 'react'
+import { useMemo } from 'react'
 import Barcode from 'react-barcode'
 
 import { useTicket } from '../../TicketContext'
@@ -10,67 +10,56 @@ interface TicketBarcodeProps {
     textColor?: TicketColor
     backgroundColor?: TicketColor
     textFontSize?: FontSize
+    maxWidth?: number
+    height?: number
 }
 
 export default function TicketBarcode({
     textColor = 'text-light',
     backgroundColor = 'background',
-    textFontSize = 'sm'
+    textFontSize = 'sm',
+    maxWidth = 180,
+    height = 30
 }: TicketBarcodeProps) {
     const { data } = useTicket()
     const value = data.barcode
-    const [cssVars, setCssVars] = useState({
-        maxWidth: 180,
-        height: 30,
-        color: '#000000',
-        background: 'transparent'
-    })
 
     const textFontSizeCss = useMemo(() => getFontSize(textFontSize), [textFontSize])
     const textColorCss = useMemo(() => getTicketTextColor(textColor), [textColor])
 
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            const root = getComputedStyle(document.documentElement)
-            setCssVars({
-                maxWidth: 180,
-                height: 30,
-                color: root.getPropertyValue(`--ticket-${textColor}`) || '#000000',
-                background: root.getPropertyValue(`--ticket-${backgroundColor}`) || 'transparent'
-            })
+    const [componentWidth, barWidth] = useMemo(() => {
+        if (!value) return [0, 0]
+
+        let barWidth = 0.6
+        const charModules = 11
+
+        let estimatedWidth = value.length * charModules * barWidth
+
+        if (estimatedWidth < maxWidth) {
+            barWidth = maxWidth / (value.length * charModules)
+            estimatedWidth = value.length * charModules * barWidth
         }
-    }, [textColor, backgroundColor])
 
-    console.log(cssVars)
+        return [estimatedWidth > maxWidth ? maxWidth : estimatedWidth, barWidth]
+    }, [value, maxWidth])
 
-    if (!value) return null
-
-    let barWidth = 0.6
-    const charModules = 11
-    const maxWidth = cssVars.maxWidth
-
-    let estimatedWidth = value.length * charModules * barWidth
-
-    if (estimatedWidth < maxWidth) {
-        barWidth = maxWidth / (value.length * charModules)
-        estimatedWidth = value.length * charModules * barWidth
+    if (!value) {
+        return null
     }
-
-    const finalWidth = estimatedWidth > maxWidth ? maxWidth : estimatedWidth
 
     return (
         <div
             className='flex flex-col items-center justify-center overflow-hidden'
-            style={{ width: `${finalWidth}px`, maxWidth: `${maxWidth}px` }}
+            style={{ width: `${componentWidth}px`, maxWidth: `${maxWidth}px` }}
         >
             <div className='overflow-hidden'>
                 <Barcode
                     value={value}
-                    height={cssVars.height}
+                    height={height}
                     displayValue={false}
                     width={barWidth}
-                    lineColor={cssVars.color}
-                    background={cssVars.background}
+                    lineColor={`var(--ticket-${textColor})`}
+                    background={`var(--ticket-${backgroundColor})`}
                     renderer='svg'
                     className='-m-2.5'
                 />
